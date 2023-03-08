d467a0a31a3f3d616ae0fde72d1c2bc8843b486e
David Opoku <71300793+dopoku12@users.noreply.github.com>
2023-03-08 00:00:14 -0500

    shadow added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,33 +1,42 @@
 const Dashboard = () => {
     return (
         <ul
-            className="
-            list-group
+            className="list-group
             d-flex 
-            flex-row "
+            flex-row 
+            rounded
+            shadow-lg"
         >
-            <li className="list-group-item ">
+            <li className="list-group-item 
+            border-top-0
+            border-bottom-0 ">
                 IP ADDRESS
                 <h3 id="address">
                     1119
                 </h3>
             </li>
 
-            <li className="list-group-item ">
+            <li className="list-group-item 
+            border-top-0
+           border-bottom-0">
                 LOCATION
                 <h3 id="location">
                     1119
                 </h3>
             </li>
 
-            <li className="list-group-item ">
+            <li className="list-group-item 
+            border-top-0
+           border-bottom-0">
                 TIMEZONE
                 <h3 id="timezone">
                     1119
                 </h3>
             </li>
 
-            <li className="list-group-item ">
+            <li className="list-group-item 
+            border-top-0
+           border-bottom-0">
                 ISP
                 <h3 id="Isp">
                     1119
