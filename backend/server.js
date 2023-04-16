843aea003234aa30e31fecac346fda8aaabd0471
David Opoku <71300793+dopoku12@users.noreply.github.com>
2023-04-16 16:49:58 -0400

    ip api running

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,27 @@ const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 5174;
+
+const PORT = process.env.PORT || 3003;
+
+var ip_addr = '173.252.107.14'
 
 app.get("/api", (req, res) => {
-    res.send('this is the api: https://api.api-ninjas.com/v1/iplookup?address=')
-    console.log('worked')
+
+    axios({
+        method: 'get',
+        url: `https://api.api-ninjas.com/v1/iplookup?address=${ip_addr}`,
+        headers: { 'X-Api-Key': process.env.React_APP_API_KEY }
+    })
+        .then((apiRes) => {
+            console.log(apiRes.data)
+            res.json(apiRes.data)
+        })
+        .catch((err) => {
+            res.send(err)
+        })
 })
-app.listen((PORT, () => {
+
+app.listen(PORT, () => {
     console.log(`listening on:${PORT}`)
-}
-)
-) 
+}) 
