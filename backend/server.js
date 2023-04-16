453fa37f40d4a84a054c38dbfead65913b98c52a
David Opoku <71300793+dopoku12@users.noreply.github.com>
2023-04-16 02:24:38 -0400

    file name changed

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
-const PORT = process.env.PORT || 8000;
 const express = require("express");
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const PORT = process.env.PORT || 5174;
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.send('this is the api: https://api.api-ninjas.com/v1/iplookup?address=')
     console.log('worked')
 })
 app.listen((PORT, () => {
     console.log(`listening on:${PORT}`)
-})
+}
+)
 ) 
